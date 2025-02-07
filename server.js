26fb96f077e30f73a2b91682f87780f9f268719c
NikolettaLiubka <nikolettaliubka@gmail.com>
2025-02-08 00:25:40 +0200

    feat: setup server, add static files, and render index page

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import url from "url";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+})
+
+app.listen(port, ()=>{
+    console.log("Server started on port " + port);
+})
