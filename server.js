1be2d12b9cf732f9a77864bc90637351f76dee0b
NikolettaLiubka <nikolettaliubka@gmail.com>
2025-02-13 20:33:12 +0200

    feat: add forms handling logic

--- server.js
+++ server.js
@@ -2,12 +2,41 @@ import express from "express";
 
 const app = express();
 const port = 3000;
+const users = [];
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", {name: null});
+})
+
+app.post("/logIn", (req, res) => {
+    const {"log-in-email": email, "log-in-password": password} = req.body;
+
+    const user = users.find(u => u.email === email && u.password === password);
+
+    if(user){
+        res.render("home.ejs", {name: user.name});
+    }
+    else{
+        res.status(401).send("User not found!");
+    }
+})
+
+app.post("/signUp", (req, res) => {
+    const {"sign-up-email": email, "sign-up-name": name, "sign-up-password": password} = req.body;
+
+    const newUser = {email, name, password};
+    const user = users.find(u => u.email === email);
+
+    if(user){
+        res.send("User already exists!");
+    }
+    else{
+        users.push(newUser);
+        res.render("home.ejs", {name: newUser.name});
+    }
 })
 
 app.listen(port, ()=>{
